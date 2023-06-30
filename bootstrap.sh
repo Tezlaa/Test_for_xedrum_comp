70bfad95430c6504ee81af052a0d9e106b97e187
Tezla <Bogdantihenko60@gmail.com>
2023-07-01 01:08:15 +0300

    fix to amazon aws

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
 
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 
-sudo apt install curl -y
+sudo yum install curl -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-git clone https://github.com/Tezlaa/Test_for_xedrum_comp.git
+sudo git clone https://github.com/Tezlaa/Test_for_xedrum_comp.git
 
 cd Test_for_xedrum_comp
 
